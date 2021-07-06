59a91cf661411aa8c1f7037df2c1bb5d9c5d0dc9
deeghayu <deefernando6@gmail.com>
2021-07-06 22:26:36 +0530

    Update wifi_scan.py

--- wifi_scan.py
+++ wifi_scan.py
@@ -2,3 +2,9 @@ import scapy.all as scapy
 import re
 
 ip_add_range_pattern = re.compile("^:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]*$")
+
+while True:
+    ip_address_range_entered = input("\n Please enter the range of the ip addresses which you wanyt to scan (ex: 192.168.1.0/24)")
+    if ip_add_range_pattern.search(ip_address_range_entered):
+        print(f"{ip_address_range_entered} is valid")
+        break
